69dd8d6f32589af2096d2b57d93841c2959ce271
Gerome Marc Braddock <braddockgerome@gmail.com>
2018-08-11 23:30:53 +0100

    Inner board and its tests completed

--- InnerBoard.py
+++ InnerBoard.py
@@ -1,20 +1,53 @@
+WINNING_COMBINATIONS = [
+            # horizontal
+            [(0,0), (1,0), (2,0)],
+            [(0,1), (1,1), (2,1)],
+            [(0,2), (1,2), (2,2)],
+            # vertical
+            [(0,0), (0,1), (0,2)],
+            [(1,0), (1,1), (1,2)],
+            [(2,0), (2,1), (2,2)],
+            # crossed
+            [(0,0), (1,1), (2,2)],
+            [(2,0), (1,1), (0,2)]
+            ]
+
+
 class InnerBoard:
-    INNER_BOARD_HEIGHT = 3
-    INNER_BOARD_WIDTH = 3
+
+    ERROR_OCCUPIED = -1
+    ERROR_NO_ERROR = 0
 
     numberOfSpaces = 9
 
-    board = [['e', 'e', 'e'],
-                  ['e', 'e', 'e'],
-                  ['e', 'e', 'e']]
+    def __init__(self):
+        self.board = [['.', '.', '.'],
+                      ['.', '.', '.'],
+                      ['.', '.', '.']]
 
     def NumberOfSpaces(self):
         return self.numberOfSpaces
 
     def TakeTurn(self, xPos, yPos, noughtCross):
         self.numberOfSpaces -= 1
-        self.board[xPos][yPos] = noughtCross
+
+        if self.board[xPos][yPos] != '.':
+            return self.ERROR_OCCUPIED
+
+        self.board[xPos][yPos] = noughtCross.type
+
+        return self.ERROR_NO_ERROR
 
     def GetBoard(self):
         return self.board
 
+    def HasBeenWon(self):
+        for coordinates in WINNING_COMBINATIONS:
+            letters = [self.board[x][y] for x, y in coordinates]
+
+            # If all the letters match
+            if "e" not in letters:
+                if len(set(letters)) <= 1:
+                    return True
+
+        return False
--- Tests/InnerBoardTests.py
+++ Tests/InnerBoardTests.py
@@ -2,9 +2,10 @@ from unittest import TestCase
 from InnerBoard import InnerBoard
 from NoughtCross import NoughtCross
 
-emptyBoard = [['e', 'e', 'e'],
-              ['e', 'e', 'e'],
-              ['e', 'e', 'e']]
+# the 'e' stands for empty
+emptyBoard = [['.', '.', '.'],
+              ['.', '.', '.'],
+              ['.', '.', '.']]
 
 
 class InnerBoardTests(TestCase):
@@ -13,32 +14,51 @@ class InnerBoardTests(TestCase):
         self.innerBoard = InnerBoard()
 
     def test_numberOfSpaces_should_show_all_positions_available(self):
-
         self.assertEqual(9, self.innerBoard.NumberOfSpaces())
 
-    def test_take_turn_should_occupy_position_after_a_go(self):
-
+    def test_take_turn_should_a_occupy_position_after_a_go(self):
         self.innerBoard.TakeTurn(0, 0, NoughtCross('X'))
 
         self.assertEqual(8, self.innerBoard.NumberOfSpaces())
 
     def test_get_board_should_return_empty_2D_array_representing_the_board(self):
-
         self.assertEqual(emptyBoard, self.innerBoard.GetBoard())
 
     def test_take_turn_should_occupy_place_on_the_board(self):
-
         cross = NoughtCross('X')
         self.innerBoard.TakeTurn(0, 0, cross)
 
-        self.assertEqual(cross, self.innerBoard.board[0][0])
+        self.assertEqual(cross.type, self.innerBoard.board[0][0])
 
     def test_take_turn_should_another_occupy_place_on_the_board(self):
-
         nought = NoughtCross('O')
         self.innerBoard.TakeTurn(2, 2, nought)
 
-        self.assertEqual(nought, self.innerBoard.board[2][2])
+        self.assertEqual(nought.type, self.innerBoard.board[2][2])
+
+    def test_HasBeenWon_should_return_false_by_default(self):
+        self.assertEqual(False, self.innerBoard.HasBeenWon())
+
+    def test_HasBeenWon_should_return_true_if_a_player_has_3_in_a_row(self):
+        nought = NoughtCross('X')
+        self.innerBoard.board[0][0] = nought.type
+        self.innerBoard.board[1][0] = nought.type
+        self.innerBoard.board[2][0] = nought.type
+
+        self.assertEqual(True, self.innerBoard.HasBeenWon())
+
+    def test_should_return_no_error_by_default(self):
+        cross = NoughtCross('X')
+        returnedError = self.innerBoard.TakeTurn(0, 0, cross)
+
+        self.assertEqual(InnerBoard.ERROR_NO_ERROR, returnedError)
+
+    def test_should_throw_error_when_attempting_to_occupy_unavailable_space(self):
+        nought = NoughtCross('O')
+        self.innerBoard.TakeTurn(2, 2, nought)
+        returnedError = self.innerBoard.TakeTurn(2, 2, nought)
+
+        self.assertEqual(InnerBoard.ERROR_OCCUPIED, returnedError)
 
 
 '''
@@ -47,9 +67,6 @@ TestList
 -O
 -M
 -B
-should_show_available_position_when_one_space_left
 -I
-should_return_no_error_by_default_after_a_go
 -E
-should_throw_error_when_attempting_to_occupy_unavailable_space
 '''
