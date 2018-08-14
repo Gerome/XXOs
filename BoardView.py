2a8a3a1a399c81509e95df38be059e98f24b093a
Gerome Marc Braddock <BraddockG@users.noreply.github.com>
2018-08-14 18:56:19 +0100

    Created the board view and its tests
    View tests are very brittle, so almost unnecesary to have them included, better indication to see if a view is working is to run the program and visually look if the view is expected. However, these tests are included regardess.

---
+++ BoardView.py
@@ -0,0 +1,69 @@
+
+
+
+# source -> https://stackoverflow.com/questions/35091557/replace-nth-occurrence-of-substring-in-string
+def __nth_repl__(s, sub, repl, nth):
+    find = s.find(sub)
+    # if find is not p1 we have found at least one match for the substring
+    i = find != -1
+    # loop util we find the nth or we find no match
+    while find != -1 and i != nth:
+        # find + 1 means we start at the last match start index + 1
+        find = s.find(sub, find + 1)
+        i += 1
+    # if i is equal to nth we found nth matches so replace
+    if i == nth:
+        return s[:find]+repl+s[find + len(sub):]
+    return s
+
+
+def __CalculatePosition__(xPos, yPos):
+    numToReplace = str(((yPos * 3) + (xPos + 1)))
+    return numToReplace
+
+
+EMPTY_INNER_BOARD ='┌───┬───┬───┐' '\n' \
+                   '│ 1 │ 2 │ 3 │' '\n' \
+                   '├───┼───┼───┤' '\n' \
+                   '│ 4 │ 5 │ 6 │' '\n' \
+                   '├───┼───┼───┤' '\n' \
+                   '│ 7 │ 8 │ 9 │' '\n' \
+                   '└───┴───┴───┘' '\n'
+
+
+def RemoveNumbersFromString(formattedBoard):
+    for i in range(1, 10):
+        formattedBoard = formattedBoard.replace(str(i), " ")
+
+    return formattedBoard
+
+
+class BoardView:
+
+    EMPTY_BOARD = [
+        [EMPTY_INNER_BOARD, EMPTY_INNER_BOARD, EMPTY_INNER_BOARD],
+        [EMPTY_INNER_BOARD, EMPTY_INNER_BOARD, EMPTY_INNER_BOARD],
+        [EMPTY_INNER_BOARD, EMPTY_INNER_BOARD, EMPTY_INNER_BOARD]
+    ]
+
+    boardViewString = ""
+
+    def __init__(self):
+        self.boardViewString = self.EMPTY_BOARD
+
+    def UpdateBoard(self, xBoard, yBoard, xPos, yPos, playerChar):
+        numToReplace = __CalculatePosition__(xPos, yPos)
+        self.boardViewString[xBoard][yBoard] = self.boardViewString[xBoard][yBoard].replace(numToReplace, playerChar)
+
+    def GetFormattedBoard(self):
+        formattedBoard = ""
+
+        for i in range(len(self.boardViewString)):
+            for k in range(len(self.boardViewString[0][0].splitlines())):
+                for j in range(len(self.boardViewString[i])):
+                    formattedBoard += self.boardViewString[i][j].splitlines()[k]
+                formattedBoard += "\n"
+
+        return RemoveNumbersFromString(formattedBoard)
+
+
---
+++ Tests/BoardViewTests.py
@@ -0,0 +1,96 @@
+from unittest import TestCase
+
+from BoardView import BoardView
+
+EMPTY_INNER_BOARD ='┌───┬───┬───┐' '\n' \
+                   '│ 1 │ 2 │ 3 │' '\n' \
+                   '├───┼───┼───┤' '\n' \
+                   '│ 4 │ 5 │ 6 │' '\n' \
+                   '├───┼───┼───┤' '\n' \
+                   '│ 7 │ 8 │ 9 │' '\n' \
+                   '└───┴───┴───┘' '\n'
+
+EMPTY_BOARD_TO_PRINT =   '┌───┬───┬───┐┌───┬───┬───┐┌───┬───┬───┐' '\n' \
+                         '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                         '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                         '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                         '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                         '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                         '└───┴───┴───┘└───┴───┴───┘└───┴───┴───┘' '\n' \
+                         '┌───┬───┬───┐┌───┬───┬───┐┌───┬───┬───┐' '\n' \
+                         '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                         '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                         '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                         '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                         '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                         '└───┴───┴───┘└───┴───┴───┘└───┴───┴───┘' '\n' \
+                         '┌───┬───┬───┐┌───┬───┬───┐┌───┬───┬───┐' '\n' \
+                         '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                         '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                         '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                         '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                         '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                         '└───┴───┴───┘└───┴───┴───┘└───┴───┴───┘' '\n'
+
+BOARD_WITH_ONE_CHAR ='┌───┬───┬───┐┌───┬───┬───┐┌───┬───┬───┐' '\n' \
+                     '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                     '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                     '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                     '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                     '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                     '└───┴───┴───┘└───┴───┴───┘└───┴───┴───┘' '\n' \
+                     '┌───┬───┬───┐┌───┬───┬───┐┌───┬───┬───┐' '\n' \
+                     '│   │   │   ││ X │   │   ││   │   │   │' '\n' \
+                     '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                     '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                     '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                     '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                     '└───┴───┴───┘└───┴───┴───┘└───┴───┴───┘' '\n' \
+                     '┌───┬───┬───┐┌───┬───┬───┐┌───┬───┬───┐' '\n' \
+                     '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                     '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                     '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                     '├───┼───┼───┤├───┼───┼───┤├───┼───┼───┤' '\n' \
+                     '│   │   │   ││   │   │   ││   │   │   │' '\n' \
+                     '└───┴───┴───┘└───┴───┴───┘└───┴───┴───┘' '\n' \
+
+class BoardViewTests(TestCase):
+    EMPTY_BOARD = [
+        [EMPTY_INNER_BOARD, EMPTY_INNER_BOARD, EMPTY_INNER_BOARD],
+        [EMPTY_INNER_BOARD, EMPTY_INNER_BOARD, EMPTY_INNER_BOARD],
+        [EMPTY_INNER_BOARD, EMPTY_INNER_BOARD, EMPTY_INNER_BOARD]
+    ]
+
+    def setUp(self):
+        self.boardView = BoardView()
+        self.testBoard = self.EMPTY_BOARD
+
+    def test_init_should_add_empty_board_to_board_view(self):
+        self.assertEqual(BoardView.EMPTY_BOARD, self.boardView.boardViewString)
+
+    def test_UpdateBoard_should_add_a_character_to_boardViewString(self):
+        self.testBoard[1][1] = self.testBoard[1][1].replace('1', 'X')
+
+        self.boardView.UpdateBoard(1, 1, 0, 0, 'X')
+
+        self.assertEqual(self.testBoard, self.boardView.boardViewString)
+
+    def test_PrintBoard_should_print_an_empty_board(self):
+        self.assertEqual(EMPTY_BOARD_TO_PRINT, self.boardView.GetFormattedBoard())
+
+    def test_PrintBoard_should_print_the_board_with_one_character(self):
+        self.boardView.UpdateBoard(1, 1, 0, 0, 'X')
+
+        self.assertEqual(BOARD_WITH_ONE_CHAR, self.boardView.GetFormattedBoard())
+
+
+'''
+Testlist
+Z
+O
+test_PrintBoard_should_print_the_board_with_one_character
+M
+B
+I
+E
+'''
